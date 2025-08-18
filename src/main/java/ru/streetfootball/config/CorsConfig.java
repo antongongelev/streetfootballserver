e890b2ed0a36e107158f4801ab14e4925a171ce2
antongongelev <antongongelev@gmail.com>
2025-08-19 00:26:03 +0300

    1st

--- src/main/java/ru/streetfootball/config/CorsConfig.java
+++ src/main/java/ru/streetfootball/config/CorsConfig.java
@@ -10,10 +10,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:3001")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true)
                 .maxAge(3600);
     }
 
